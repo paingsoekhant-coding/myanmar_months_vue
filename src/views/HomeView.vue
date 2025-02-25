14543e5774f90a54a8b53cd0cf4539d44eaf7df6
Paing Soe Khant <babyboss7916@gmail.com>
2025-02-25 22:19:30 +0630

    add card & connect json

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from "@/components/Card.vue";
+</script>
+
+
+<template>
+  <div class="container p-7">
+    <div class="row">
+      <h1 class="text-2xl text-center m-4 antialiased">မြန်မာလများ</h1>
+    </div>
+
+    <div class="row flex flex-wrap justify-around">
+      <Card></Card>
+    </div>
+  </div>
+</template>
+
+
+<style>
+</style>
